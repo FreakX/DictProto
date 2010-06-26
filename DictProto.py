32bf5a7476ca8632511712cf8b64568c127511c4
FreakX <dani11795@googlemail.com>
2010-06-26 14:29:00 +0200

    DictProto_v1.2 Fixx all issues
    Signed-off-by: FreakX <dani11795@googlemail.com>

--- DictProto.py
+++ DictProto.py
@@ -1,4 +1,5 @@
 import socket
+import pickle
 
 
 SERVER_SOCK = 0x00
@@ -14,9 +15,22 @@ class Socket(object):
             self.socket.connect((host, port))
         self.socktype = socktype
         self.port = port
-        self.protoverstr = "DictProto_v1.1"
+        self.protoverstr = "DictProto_v1.2"
     def __del__(self):
-        self.socket.close()
+        if self.socktype == ClIENT_SOCK:
+            try:
+                self.socket.send("E:E")
+            except: pass
+            try:
+                self.socket.close()
+            except: pass
+        if self.socktype == SERVER_SOCK:
+            try:
+                self.conn.send("E:E")
+            except: pass
+            try:
+                self.conn.close()
+            except: pass
     def Listen(self):
         self.socket.listen(1)
         self.conn, addr = self.socket.accept()
@@ -24,34 +38,9 @@ class Socket(object):
 
         
     def Send(self, datadict):
-        packet = []
-        packet.append(self.protoverstr)
-        for x in datadict:
-            keylen = hex(len(str(x)))[2:]
-            if len(keylen) == 1:
-                packet.append("0")
-                packet.append(keylen)
-            else:
-                packet.append(keylen)
-                
-            key = str(x)
-            packet.append(key)
-            
-            datalen = hex(len(str(datadict[x])))[2:]
-            if len(datalen) == 1:
-                packet.append("00")
-                packet.append(datalen)
-            elif len(datalen) == 2:
-                packet.append("0")
-                packet.append(datalen)
-            else:
-                packet.append(datalen)
-                
-            data = str(datadict[x])
-            packet.append(data)
-            
-        if self.socktype == CLIENT_SOCK: self.socket.send(''.join(packet))
-        if self.socktype == SERVER_SOCK: self.conn.send(''.join(packet))
+        packet = self.protoverstr + pickle.dumps(datadict)
+        if self.socktype == CLIENT_SOCK: self.socket.send(packet)
+        if self.socktype == SERVER_SOCK: self.conn.send(packet)
         
     def Receive(self):
         while True:
@@ -59,25 +48,9 @@ class Socket(object):
             if self.socktype == CLIENT_SOCK: data = self.socket.recv(4096)
             if self.protoverstr in data:
                 data = data.split(self.protoverstr)[1]
-                returndict = {}
-                offset = 0
-                while True:
-                    try:
-                        keylen = int(data[offset:offset+2], 16)
-                        offset +=2
-                        key = data[offset:offset+keylen]
-                        offset +=keylen
-                        datalen = int(data[offset:offset+3], 16)
-                        offset +=3
-                        pdata = data[offset:offset+datalen]
-                        offset +=datalen
-
-
-                        
-                        returndict[key] = pdata
-
-                    except:
-                        break
+                returndict = pickle.loads(data)
                 return returndict
+            if data == "E:E":
+                self.__del__()
                     
                 
